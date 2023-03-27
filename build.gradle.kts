29764eed0d512644e2764dc1709762933840b491
Joseph Lee <joseph@jc-lab.net>
2023-03-27 15:43:30 +0900

    chore: migrate to gradle and jclab repo

---
+++ build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    `signing`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.5")
+}
+
+group = "kr.jclab.javautils"
+version = Version.PROJECT
+description = "routd"
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("secure ipc")
+                url.set("https://github.com/jc-lab/sipc")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jclab")
+                        name.set("Joseph Lee")
+                        email.set("joseph@jc-lab.net")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/jc-lab/sipc.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/jc-lab/sipc.git")
+                    url.set("https://github.com/jc-lab/sipc")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if ("$version".endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = findProperty("ossrhUsername") as String?
+                password = findProperty("ossrhPassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { project.hasProperty("signing.gnupg.keyName") || project.hasProperty("signing.keyId") }
+}
